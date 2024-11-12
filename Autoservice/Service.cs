e1a23e2403855b5527cd5d31ee9a7a02e44fbee7
Раис Хасанов <khasanov-rkh@mail.ru>
2024-11-12 18:40:50 +0500

    ЛБ 8 (1)

--- Autoservice/Service.cs
+++ Autoservice/Service.cs
@@ -11,7 +11,7 @@ namespace Autoservice
 {
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Service
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -20,7 +20,7 @@ namespace Autoservice
             this.ClientService = new HashSet<ClientService>();
             this.ServicePhoto = new HashSet<ServicePhoto>();
         }
-    
+
         public int ID { get; set; }
         public string Title { get; set; }
         public string MainImagePath { get; set; }
@@ -43,7 +43,7 @@ namespace Autoservice
             }
         }
         public string Description { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ClientService> ClientService { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
--- Autoservice/ServicePage.xaml.cs
+++ Autoservice/ServicePage.xaml.cs
@@ -260,6 +260,9 @@ namespace Autoservice
             ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
         }
 
-  
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new SignUpPage((sender as Button).DataContext as Service));
+        }
     }
 }
